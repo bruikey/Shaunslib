91c722f570fefeca3292cec6c3abfdd0809da9cc
bruikey <bruikey03@gmail.com>
2023-08-24 20:50:42 +0530

    postgresql changes

--- app.py
+++ app.py
@@ -1,33 +1,44 @@
 from flask import Flask, render_template, request, redirect, url_for
-from models import *
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bruikey03:cigaftersex@34.93.154.219:5432/users'
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://bruikey03:cigaftersex@34.93.154.219:5432/users"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-db.init_app(app)
+app.app_context().push()
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+    # The constructor name should be "__init__" instead of "__init"
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
-
 @app.route('/signup', methods=['POST', 'GET'])
-def signup_send():
-
+def signup():
     if request.method == 'POST':
-        username = request.form.get('username')
-        email = request.form.get('email')
-        password = request.form.get('password')
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
 
-        new_user = User(username=username, email=email, password=password)
+        new_user = User(username, email, password)
         db.session.add(new_user)
         db.session.commit()
-
+     
         return redirect(url_for('index'))
     return render_template('signup.html')
-
+    
 if __name__ == "__main__":
     app.run(debug=True)
