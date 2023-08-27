8554e3ffcd4b2e1f8b0fb35a33c2b8738bae3309
bruikey <bruikey03@gmail.com>
2023-08-27 20:43:30 +0530

    changes

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git add .
-git commit -m "#addition"
-git push
-
-ssh -i ~/.ssh/id_rsa bruikey03@34.100.160.119 'cd /home/bruikey03/shaunslib && git pull && sudo systemctl restart shaunslib nginx'
